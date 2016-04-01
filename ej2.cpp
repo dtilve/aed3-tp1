2da343dd47e8e111e38c56096193d32dd0c31fc5
martinlabate <martinlabate@gmail.com>
2016-04-01 17:31:19 -0300

    ej2 andando =D

--- ej2.cpp
+++ ej2.cpp
@@ -1,33 +1,63 @@
+#include <tuple>
+#include <vector>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+void genkidama(int, int, vector<tuple<int, int>>);
+
 int main(){
-//	genkidama();
+	int t;
+	int n;
+	cin >> n >> t;
+	vector<tuple<int,int>> e;
+	tuple<int,int> en;
+	int x;
+	int y;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		en = make_tuple(x,y);
+		e.push_back((en));
+	}
+	genkidama(t, n, e);
 	return 0;
 }
 
-void genkidama(int T, int N, E enemigos){
-	conj atacados;
-	int j = 1;
-	obj = E[0];
-	genki = 0;
-	while(j <= N){
-
-		while((j <= N) && (E[j][y] + T <= obj[y])){
-			j++;
+void genkidama(int t, int n, vector<tuple<int,int>> e){
+	assert (n > 0 && n == e.size() && "La cantidad de enemigos es distinta a la cantidad de posisciones");
+	vector<int> atacados;
+	int j = n - 2;
+	tuple<int,int> obj = e[n-1];
+	int genki = 0;
+	bool hayAlgunoVivo = true;
+	while(hayAlgunoVivo){
+		while((j >= 0) && ((get<1>(e[j]) + t) >= get<1>(obj))){
+			j--;
 		}
-		atacados.add(E[j-1]);
+		atacados.push_back(j + 2);
 		genki ++;
-		dis = E[j-1][x] + T;
-		i = j
-		while((i <= N) && (E[i][x] <= dis)){
-			i++;
+		if (j < 0) {
+			hayAlgunoVivo = false;
 		}
-		if (E[i][x] <= dis){
-			j = N;
+		int dis = get<0>(e[j + 1]) + t;
+		int i = j;
+		while((i >= 0) && (get<0>(e[i]) <= dis)){
+			i--;
 		}
-
-		obj = E[i];
+		if (get<0>(e[i]) <= dis){
+			hayAlgunoVivo = false;
+		}
+		obj = e[i];
+		j = i;
 	}
 
-		//print  de henki
-		//print del conjunto de enemigos atacados 
-
+		std::cout << genki << std::endl;
+		int h = 0;
+		while (h < genki) {
+				std::cout << atacados[h];
+				h++;
+				if (h < genki) {
+						std::cout << " ";
+				}
+		}
 }
