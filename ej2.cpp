6d60fa18dfb62c1a3338ae0189853c43d423aa1a
Sebivt <sebi_282@hotmail.com>
2016-04-03 21:29:35 -0300

    Código del 2 modularizado pero creo que no andando

--- ej2.cpp
+++ ej2.cpp
@@ -5,6 +5,9 @@
 using namespace std;
 
 void genkidama(int, int, vector<tuple<int, int>>);
+int indiceDeMenorYQueLoMata(int, int, vector<tuple<int,int>>);
+int indiceDeMayorXQueMata(int, int, vector<tuple<int,int>>);
+void destruir();
 
 int main(){
 	int t;
@@ -23,6 +26,62 @@ int main(){
 	return 0;
 }
 
+// Toma el t, el indice del objetivo en e y el vector de tuplas.
+int indiceDeMenorYQueLoMata(int t, int indiceDeObjetivo, vector<tuple<int,int>> e){
+	int j = indiceDeObjetivo - 1;
+	while((j >= 0) && ((get<1>(e[j]) + t) >= get<1>(e[indiceDeObjetivo]))){
+		j--;
+	}
+	j++;
+	return j;
+}
+
+int indiceDeMayorXQueMata(int t, int indiceDeObjetivo, vector<tuple<int,int>> e){
+	int distanciaDeDano = get<0>(e[indiceDeObjetivo]) + t;
+	int i = indiceDeObjetivo - 1;
+	while((i >= 0) && (get<0>(e[i]) <= distanciaDeDano)){
+		i--;
+	}
+	i++;
+	return i;
+}
+
+void destruir(int t, int indiceDeObjetivo, vector<tuple<int,int>> e, int genkidamas, int indicePorArea){
+	indicePorArea = indiceDeMayorXQueMata(t, indiceDeObjetivo, e) - 1;
+	genkidamas++;
+}
+
+
+// MODULARIZADO
+void genkidama(int t, int n, vector<tuple<int,int>> e){
+	assert (n > 0 && n == e.size() && "La cantidad de enemigos es distinta a la cantidad de posisciones");
+	vector<int> atacados;
+	int genkidamasUtilizadas = 0;
+	int indiceDeObjetivoPorArea = n-1;
+	// el de arriba es aquel al que quiero que le llegue la onda expansiva
+	bool hayAlgunoVivo = true;
+	while(hayAlgunoVivo){
+		// el de abajo es aquel al que le voy a tirar la bomba
+		int indiceDeObjetivo = indiceDeMenorYQueLoMata(t, indiceDeObjetivoPorArea, e);
+		atacados.push_back(indiceDeObjetivo + 1);
+		// el +1 de arriba surge de que en el enunciado se enumeran desde el 1 y
+		// nosotros enumeramos desde 0
+		hayAlgunoVivo = !(indiceDeMayorXQueMata(t, indiceDeObjetivo, e) == 0);
+		destruir(t, indiceDeObjetivo, e, genkidamasUtilizadas, indiceDeObjetivoPorArea);
+	}
+		std::cout << genkidamasUtilizadas << std::endl;
+		int h = 0;
+		while (h < genkidamasUtilizadas) {
+				std::cout << atacados[h];
+				h++;
+				if (h < genkidamasUtilizadas) {
+						std::cout << " ";
+				}
+		}
+}
+
+
+/* ORIGINAL QUE ANDA
 void genkidama(int t, int n, vector<tuple<int,int>> e){
 	assert (n > 0 && n == e.size() && "La cantidad de enemigos es distinta a la cantidad de posisciones");
 	vector<int> atacados;
@@ -44,6 +103,8 @@ void genkidama(int t, int n, vector<tuple<int,int>> e){
 		while((i >= 0) && (get<0>(e[i]) <= dis)){
 			i--;
 		}
+		// Si el de mas a la derecha tambien fuera a ser matado, el while
+		// de arriba da i igual a -1 y lo de abajo se indefine :(
 		if (get<0>(e[i]) <= dis){
 			hayAlgunoVivo = false;
 		}
@@ -61,3 +122,4 @@ void genkidama(int t, int n, vector<tuple<int,int>> e){
 				}
 		}
 }
+*/
