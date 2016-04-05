6bdfb1b24fe41883438fca5587994b7117a3aa06
martinlabate <martinlabate@gmail.com>
2016-04-05 14:45:55 -0300

    volvi atras a la forma no modularizada, proq hay problemas para pasar como parametro matrices no inicializadas =/

--- ej1.cpp
+++ ej1.cpp
@@ -3,10 +3,24 @@
 #include <iostream>
 using namespace std;
 
-	//int n = 5;
-	void kaioKen(int);
-	void inicializarMatriz(int, int, int[][n]);
-	void distribuirGuerreros(int, int, int[][n]);
+// template <int filas, int columnas>
+// class Matrix
+// {
+// 	int m [filas][columnas];
+// 	Matrix();
+// };
+// template <int filas, int columnas>
+// Matrix<filas, columnas>::Matrix(){
+// 	for (int h = 0; h < filas; h++){
+// 		for (int q = 0; q < columnas; q++){
+// 			this[h][q] = 0;
+// 		}
+// 	}
+// }
+
+void kaioKen(int);
+// void inicializarMatriz(int, int, Matrix<int,int>);
+// void distribuirGuerreros(int, int, Matrix<);
 
 int main(int argc, char* argv[]){
 	cerr << 11<<endl;
@@ -17,75 +31,76 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
-void inicializarMatriz(int filas, int columnas, int[][n] m){
-	for (int h = 0; h < filas; h++){
-		for (int q = 0; q < columnas; q++){
-			m[h][q] = 0;
-		}
-	}
-}
+// void inicializarMatriz(int filas, int columnas, int** m){
+// 	std::cerr << "empieza a inicializar?" << std::endl;
+// 	for (int h = 0; h < filas; h++){
+// 		for (int q = 0; q < columnas; q++){
+// 			m[h][q] = 0;
+// 		}
+// 	}
+// 	std::cerr << "terminana de inicializar?" << std::endl;
+// }
+//
+// void distribuirGuerreros(int filas, int columnas, int** m){
+// 	int i = 0;
+// 	int j;
+// 	int cambio = 0;
+// 	int equipo = 1;
+// 	int x = 0;
+// 	while(i < filas){
+// 		cambio = pow(2,i);
+// 		x = 0;
+// 		j = 0;
+// 		while(j < columnas){
+// 			if(x < cambio){
+// 				m[i][j] = equipo;
+// 			}else{
+// 				equipo = ((equipo +1) % 2);
+// 				m[i][j] = equipo;
+// 				x = 0;
+// 			}
+// 			j++;
+// 			x++;
+// 		}
+// 		i++;
+// 	}
+// }
+//
+// void kaioKen(int n){
+//
+// 	int filas = ceil(log2(n));
+// 	std::cerr << "Entra a kaioken?" << std::endl;
+// 	inicializarMatriz(filas, n, m);
+// 	distribuirGuerreros(filas, n, m);
+//
+// 	int i = 0;
+// 	int j;
+// 	cout << filas << endl;
+// 	while(i < filas){
+// 		j = 0;
+// 		while(j < n){
+// 			cout << m[i][j]+1;
+// 			cerr << m[i][j]+1;
+// 			j ++;
+// 			if (j < n) {
+// 				cout << " ";
+// 				cerr << " ";
+// 			}
+// 		}
+// 		cout << endl;
+// 		cerr << endl;
+// 		i++;
+// 	}
+// }
 
-void distribuirGuerreros(int filas, int columnas, int[][n] m){
-	int i = 0;
-	int j;
-	int cambio = 0;
-	int equipo = 1;
-	int x = 0;
-	while(i < filas){
-		cambio = pow(2,i);
-		x = 0;
-		j = 0;
-		while(j < columnas){
-			if(x < cambio){
-				m[i][j] = equipo;
-			}else{
-				equipo = ((equipo +1) % 2);
-				m[i][j] = equipo;
-				x = 0;
-			}
-			j++;
-			x++;
-		}
-		i++;
-	}
-}
-
+// VERSIÓN NO MODULARIZADA QUE ANDA
 void kaioKen(int n){
-	std::cerr << "Entra a la funcion?" << std::endl;
-
-	int filas = ceil(log2(n));
-	int m[filas][n];
-
-	inicializarMatriz(filas, n, m[][n]);
-	distribuirGuerreros(filas, n, m[][n]);
-
-	int i = 0;
-	int j;
-	cout << filas << endl;
-	while(i < filas){
-		j = 0;
-		while(j < n){
-			cout << m[i][j]+1;
-			j ++;
-			if (j < n) {
-				std::cout << " ";
-			}
-		}
-		cout << endl;
-		i++;
-	}
-}
-
-/* VERSIÓN NO MODULARIZADA QUE ANDA
-void kaioKen(int n){
-	std::cerr << "Entra a la funcion?" << std::endl;
-	int i = 0;
-	int j = 0;
 	int filas = ceil(log2(n));
 	int equipo = 1;
 	int cambio = 0;
 	int x = 0;
 	int m [filas][n];
+	//Inicializamos la matriz en 0
 	for (int h = 0; h < filas; ++h)
 	{
 		for (int q = 0; q < n; ++q)
@@ -93,11 +108,11 @@ void kaioKen(int n){
 			m[h][q] = 0;
 		}
 	}
-	while(i < filas){
+	//Distribuimos los guerreros
+	for (int i = 0; i < filas; i++){
 		cambio = pow(2,i);
 		x = 0;
-		j = 0;
-		while(j < n){
+		for(int j = 0; j < n; j++){
 			if(x < cambio){
 				m[i][j] = equipo;
 			}else{
@@ -105,23 +120,18 @@ void kaioKen(int n){
 				m[i][j] = equipo;
 				x = 0;
 			}
-			j++;
 			x++;
 		}
-		i++;
 	}
-	i = 0;
+
 	cout << filas << endl;
-	while(i < filas){
-		j = 0;
-		while(j < n){
+	for(int i = 0; i < filas; i++){
+		for(int j = 0; j < n; j++){
 			cout << m[i][j]+1;
-			j ++;
 			if (j < n) {
 				std::cout << " ";
 			}
 		}
 		cout << endl;
-		i++;
 	}
-}*/
+}
