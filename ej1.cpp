e334a89f07b81276d18890cace12036d5604b989
Sebivt <sebi_282@hotmail.com>
2016-04-05 01:49:54 -0300

    Ej 1 modularizado pero no andando por algún motivo de la vida (?)

--- ej1.cpp
+++ ej1.cpp
@@ -3,7 +3,10 @@
 #include <iostream>
 using namespace std;
 
-	void kaioKen(int n);
+	//int n = 5;
+	void kaioKen(int);
+	void inicializarMatriz(int, int, int[][n]);
+	void distribuirGuerreros(int, int, int[][n]);
 
 int main(int argc, char* argv[]){
 	cerr << 11<<endl;
@@ -14,6 +17,66 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
+void inicializarMatriz(int filas, int columnas, int[][n] m){
+	for (int h = 0; h < filas; h++){
+		for (int q = 0; q < columnas; q++){
+			m[h][q] = 0;
+		}
+	}
+}
+
+void distribuirGuerreros(int filas, int columnas, int[][n] m){
+	int i = 0;
+	int j;
+	int cambio = 0;
+	int equipo = 1;
+	int x = 0;
+	while(i < filas){
+		cambio = pow(2,i);
+		x = 0;
+		j = 0;
+		while(j < columnas){
+			if(x < cambio){
+				m[i][j] = equipo;
+			}else{
+				equipo = ((equipo +1) % 2);
+				m[i][j] = equipo;
+				x = 0;
+			}
+			j++;
+			x++;
+		}
+		i++;
+	}
+}
+
+void kaioKen(int n){
+	std::cerr << "Entra a la funcion?" << std::endl;
+
+	int filas = ceil(log2(n));
+	int m[filas][n];
+
+	inicializarMatriz(filas, n, m[][n]);
+	distribuirGuerreros(filas, n, m[][n]);
+
+	int i = 0;
+	int j;
+	cout << filas << endl;
+	while(i < filas){
+		j = 0;
+		while(j < n){
+			cout << m[i][j]+1;
+			j ++;
+			if (j < n) {
+				std::cout << " ";
+			}
+		}
+		cout << endl;
+		i++;
+	}
+}
+
+/* VERSIÓN NO MODULARIZADA QUE ANDA
 void kaioKen(int n){
 	std::cerr << "Entra a la funcion?" << std::endl;
 	int i = 0;
@@ -61,4 +124,4 @@ void kaioKen(int n){
 		cout << endl;
 		i++;
 	}
-}
+}*/
