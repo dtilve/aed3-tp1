52a175eeffa102c8918a2163c556bafe05f29568
NataliaP1 <NataliaP1@users.noreply.github.com>
2016-04-08 17:53:17 -0300

    Update ej1.cpp

--- ej1.cpp
+++ ej1.cpp
@@ -1,16 +1,14 @@
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
+#include<cstdio>
 using namespace std;
-
 
 void kaioKen(int);
 
 int main(int argc, char* argv[]){
-	cerr << 11<<endl;
 	int i;
 	cin >> i;
-	cerr << 22 <<endl;
 	kaioKen(i);
 	return 0;
 }
@@ -21,7 +19,15 @@ void kaioKen(int n){
 	int cambio = 0;
 	int x = 0;
 	int m [filas][n];
-
+	//Inicializamos la matriz en 0
+	for (int h = 0; h < filas; ++h)
+	{
+		for (int q = 0; q < n; ++q)
+		{
+			m[h][q] = 0;
+		
+	}
+	}
 	//Distribuimos los guerreros
 	for (int i = 0; i < filas; i++){
 		cambio = pow(2,i);
@@ -37,7 +43,6 @@ void kaioKen(int n){
 			x++;
 		}
 	}
-
 	cout << filas << endl;
 	for(int i = 0; i < filas; i++){
 		for(int j = 0; j < n; j++){
@@ -49,3 +54,4 @@ void kaioKen(int n){
 		cout << endl;
 	}
 }
+
